82abef99f351e33143cb2db1da6d359bc149ad5f
Damian Vega Meza <damian@LAPTOP-H44S0OTB.localdomain>
2026-03-12 11:22:35 -0700

    Quiz doesn't pass pytest

--- main.cpp
+++ main.cpp
@@ -6,17 +6,21 @@ using namespace std;
 int main()
 {
     double a, b, c;
+    double root1, root2;
     // TODO
+    cout << "Enter three numbers" << endl;
+    cin >> a >> b >> c;
+    // TODO
+    root1 = (-b + sqrt(b*b - 4*a*c)) / (2*a);
+    root2 = (-b - sqrt(b*b - 4*a*c)) / (2*a);
 
-    // TODO
-    // TODO
-    // TODO
-
-    // TODO
-    // TODO
 
     cout << setprecision(2) << fixed;
-    // TODO
-
+    if(root1 < root2) {
+        cout << root1 << " " << root2 << endl;
+        
+    } else {
+        cout << root2 << " " << root1 << endl;
+    }
     return 0;
 }
