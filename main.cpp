9644d663ae559927a0849183162eb45e5ca9845f
Damian Vega Meza <damian@LAPTOP-H44S0OTB.localdomain>
2026-03-12 12:35:54 -0700

    Quiz done

--- main.cpp
+++ main.cpp
@@ -3,24 +3,19 @@
 #include <cmath>
 using namespace std;
 
-int main()
-{
+int main() {
     double a, b, c;
     double root1, root2;
-    // TODO
-    cout << "Enter three numbers" << endl;
+
     cin >> a >> b >> c;
-    // TODO
     root1 = (-b + sqrt(b*b - 4*a*c)) / (2*a);
     root2 = (-b - sqrt(b*b - 4*a*c)) / (2*a);
 
-
     cout << setprecision(2) << fixed;
     if(root1 > root2) {
-        cout << root1 << " " << root2 << endl;
-        
+        cout << root1 << " " << root2 << '\n';
     } else {
-        cout << root2 << " " << root1 << endl;
+        cout << root2 << " " << root1 << '\n';
     }
     return 0;
 }
